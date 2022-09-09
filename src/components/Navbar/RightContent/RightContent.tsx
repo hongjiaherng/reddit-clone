62a4f5f4652ad52a13f57a87b0c969bbca9037cf
hongjiaherng <jiaherng2001@gmail.com>
2022-09-09 14:00:06 +0800

    First commit

---
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -0,0 +1,25 @@
+import { Button, Flex } from "@chakra-ui/react";
+import React from "react";
+import AuthModal from "../../Modal/Auth/AuthModal";
+import AuthButtons from "./AuthButtons";
+import { signOut, User } from "firebase/auth";
+import { auth } from "../../../firebase/clientApp";
+import Icons from "./Icons";
+import UserMenu from "./UserMenu";
+
+type RightContentProps = {
+	user?: User | null; // undefined, user, or null
+};
+
+const RightContent: React.FC<RightContentProps> = ({ user }) => {
+	return (
+		<>
+			<AuthModal />
+			<Flex justify="center" align="center">
+				{user ? <Icons /> : <AuthButtons />}
+				<UserMenu user={user} />
+			</Flex>
+		</>
+	);
+};
+export default RightContent;
